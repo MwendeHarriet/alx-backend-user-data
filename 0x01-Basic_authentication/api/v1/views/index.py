76c62d80a0a90452951d1eecad1b455ad167018e
MwendeHarriet <mwendeharriet@gmail.com>
2024-04-16 18:40:13 +0000

    app and index

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,21 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    GET /api/v1/unauthorized
+    Raise 401 error using abort
+    """
+    abort(401, description="Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    Raise a 403 error using abort
+    """
+    abort(403, description="Forbidden")
