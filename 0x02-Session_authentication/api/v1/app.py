0cd10076299c5d4b2a3ac1f402acea5063c60a4e
MwendeHarriet <mwendeharriet@gmail.com>
2024-04-18 14:48:31 +0000

    modify user and app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -21,6 +21,15 @@ if auth_type == "auth":
 elif auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth_type == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif auth_type == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.before_request
@@ -34,12 +43,15 @@ def bef_req():
         excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and\
+             auth.session_cookie(request) is None:
                 abort(401, description="Unauthorized")
-            if auth.current_user(request) is None:
+            request.current_user = auth.current_user(request)
+            if request.current_user is None:
                 abort(403, description="Forbidden")
 
 
