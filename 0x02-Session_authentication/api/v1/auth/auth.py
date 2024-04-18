c76acae1aa6040879b5454e3065ca60635185a91
MwendeHarriet <mwendeharriet@gmail.com>
2024-04-18 15:28:55 +0000

    auth

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth:
@@ -34,3 +35,10 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Public method: current_user"""
         return None
+
+    def session_cookie(self, request=None):
+        """Returns a cookie value from a request"""
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
